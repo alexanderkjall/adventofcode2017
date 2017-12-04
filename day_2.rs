f05ac1a09b1f89f11b25d45675918045508dab29
Alexander Kjäll <alexander.kjall@gmail.com>
2017-12-04 21:09:43 +0100

    solution to day 2 part 2

--- day_2.rs
+++ day_2.rs
@@ -8,17 +8,29 @@ fn main() {
         .expect("something went wrong reading the file");
 
     let mut sum_part_1 = 0;
+    let mut sum_part_2 = 0;
 
     let rows = contents.split("\n");
     for row in rows {
         if row.len() == 0 {
             continue;
         }
-        let max = row.split_whitespace().map(|x| x.parse::<i32>().unwrap()).max().unwrap();
-        let min = row.split_whitespace().map(|x| x.parse::<i32>().unwrap()).min().unwrap();
+        let nums = row.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        let max = nums.iter().max().unwrap();
+        let min = nums.iter().min().unwrap();
         sum_part_1 += max - min;
+
+        for n1 in nums.iter() {
+            for n2 in nums.iter() {
+                if n1 != n2 && n1 % n2 == 0 {
+                    sum_part_2 += n1 / n2;
+                }
+            }
+        }
+
     }
 
     println!("answer day2 part1 = {0}", sum_part_1);
+    println!("answer day2 part2 = {0}", sum_part_2);
 
 }
