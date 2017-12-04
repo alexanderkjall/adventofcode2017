f89cf9fbbdf2e44d2286ab18109605d334308db4
Alexander Kjäll <alexander.kjall@gmail.com>
2017-12-04 17:30:57 +0100

    solution day 1 part 2

--- day_1.rs
+++ day_1.rs
@@ -9,21 +9,27 @@ fn main() {
 
     contents.pop();
 
-    let mut sum = 0;
+    let mut sum_part_1 = 0;
+    let mut sum_part_2 = 0;
 
     const RADIX: u32 = 10;
     for (i, c) in contents.chars().enumerate() {
-        let next_c;
+        let next_c_1;
+        let next_c_2 = contents.chars().nth((i + (contents.len() / 2)) % contents.len()).unwrap();
         if i + 1 != contents.len() {
-            next_c = contents.chars().nth(i + 1).unwrap();
+            next_c_1 = contents.chars().nth(i + 1).unwrap();
         } else {
-            next_c = contents.chars().nth(0).unwrap();
+            next_c_1 = contents.chars().nth(0).unwrap();
         }
 
-        if c == next_c {
-            sum += c.to_digit(RADIX).unwrap();
+        if c == next_c_1 {
+            sum_part_1 += c.to_digit(RADIX).unwrap();
+        }
+        if c == next_c_2 {
+            sum_part_2 += c.to_digit(RADIX).unwrap();
         }
 
     }
-    println!("answer day1 part1 = {0}", sum);
+    println!("answer day1 part1 = {0}", sum_part_1);
+    println!("answer day1 part2 = {0}", sum_part_2);
 }
