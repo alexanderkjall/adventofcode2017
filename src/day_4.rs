b882ffcc16a2289625fce7eba7e59885bf4a0e9b
Alexander Kjäll <alexander.kjall@gmail.com>
2017-12-05 21:07:20 +0100

    solution to day 4 part 2

--- src/day_4.rs
+++ src/day_4.rs
@@ -8,6 +8,7 @@ fn main() {
         .expect("something went wrong reading the file");
 
     let mut sum_part_1 = 0;
+    let mut sum_part_2 = 0;
 
     let rows = contents.split("\n");
 
@@ -17,14 +18,23 @@ fn main() {
         }
         let words = row.split_whitespace().collect::<Vec<&str>>();
         let mut unique_words = std::collections::HashSet::new();
+        let mut unique_sorted_words = std::collections::HashSet::new();
 
         for w in words.iter() {
             unique_words.insert(w);
+            let mut chars: Vec<char> = w.chars().collect();
+            chars.sort_by(|a, b| b.cmp(a));
+            unique_sorted_words.insert(chars);
         }
 
         if words.len() == unique_words.len() {
             sum_part_1 += 1;
         }
+
+        if words.len() == unique_sorted_words.len() {
+            sum_part_2 += 1;
+        }
     }
     println!("answer day4 part1 = {0}", sum_part_1);
+    println!("answer day4 part2 = {0}", sum_part_2);
 }
