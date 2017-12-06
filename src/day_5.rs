f8e1c8bfb6d17e563b050152e8286c12a7382110
Alexander Kjäll <alexander.kjall@gmail.com>
2017-12-06 20:12:32 +0100

    solution to day 5 part 1

---
+++ src/day_5.rs
@@ -0,0 +1,34 @@
+use std::io::prelude::*;
+
+fn jump_around(mut instruction: Vec<i32>) -> i32 {
+    let mut pos: i32 = 0;
+    let mut step: i32 = 0;
+    while pos < instruction.len() as i32 {
+        let p = instruction[pos as usize].clone();
+        instruction[pos as usize] = instruction[pos as usize] + 1;
+        pos = pos + p;
+        step += 1;
+    }
+    return step;
+}
+
+fn main() {
+    let mut f = std::fs::File::open("day5-input").expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let rows = contents.split("\n");
+
+    let mut instruction: Vec<i32> = vec![];
+
+    for row in rows {
+        if row.len() == 0 {
+            continue;
+        }
+        instruction.push(row.parse::<i32>().unwrap())
+    }
+
+    println!("answer day5 part1 = {0}", jump_around(instruction));
+}
