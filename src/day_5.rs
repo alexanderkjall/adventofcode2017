0cb311c7c534cda6bc2cf00e9ccfab6ebee493e2
Alexander Kjäll <alexander.kjall@gmail.com>
2017-12-06 20:19:54 +0100

    solution to day 5 part 2

--- src/day_5.rs
+++ src/day_5.rs
@@ -12,6 +12,22 @@ fn jump_around(mut instruction: Vec<i32>) -> i32 {
     return step;
 }
 
+fn jump_around_type_2(mut instruction: Vec<i32>) -> i32 {
+    let mut pos: i32 = 0;
+    let mut step: i32 = 0;
+    while pos < instruction.len() as i32 {
+        let p = instruction[pos as usize].clone();
+        if p > 2 {
+            instruction[pos as usize] -= 1;
+        } else {
+            instruction[pos as usize] += 1;
+        }
+        pos = pos + p;
+        step += 1;
+    }
+    return step;
+}
+
 fn main() {
     let mut f = std::fs::File::open("day5-input").expect("file not found");
 
@@ -30,5 +46,6 @@ fn main() {
         instruction.push(row.parse::<i32>().unwrap())
     }
 
-    println!("answer day5 part1 = {0}", jump_around(instruction));
+    println!("answer day5 part1 = {0}", jump_around(instruction.clone()));
+    println!("answer day5 part2 = {0}", jump_around_type_2(instruction));
 }
