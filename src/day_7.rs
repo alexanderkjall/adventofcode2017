7d0468e6590340658fea5a93b08a913583b948bc
Alexander Kjäll <alexander.kjall@gmail.com>
2017-12-19 22:39:28 +0100

    solution to day 7 part 2

--- src/day_7.rs
+++ src/day_7.rs
@@ -32,6 +32,49 @@ impl Program {
     }
 }
 
+fn calc_weight(p: &String, programs: &HashMap<String, Program>, lvl: i32) -> i32 {
+    let mut w = 0;
+
+    let mut weights = HashMap::new();
+    let mut weight_nums = HashMap::new();
+    let mut weight_names = HashMap::new();
+    for child in &programs[p].carry {
+        let cw = calc_weight(child, &programs, lvl + 1);
+/*
+        for _ in 0..lvl {
+            print!("    ");
+        }
+        println!("{0} has weight {1}", child, cw);
+*/
+        weights.insert(child.clone(), cw);
+        let k = weight_nums.get(&cw).unwrap_or(&0) + 1;
+        weight_nums.insert(cw, k);
+        weight_names.insert(cw, child);
+        w += cw;
+    }
+    if weight_nums.len() > 1 {
+        let mut correct_val = 0;
+        let mut wrong_val = 0;
+        for (val, count) in weight_nums.clone() {
+            if count != 1 {
+                correct_val = val;
+            } else {
+                wrong_val = val;
+            }
+        }
+        for (val, count) in weight_nums {
+            let prog = &programs[weight_names.get(&val).unwrap().clone()];
+            if count != 1 {
+            } else {
+                println!("answer day7 part2 = {0}", prog.weight - (wrong_val - correct_val));
+            }
+        }
+    }
+    w += programs[p].weight;
+
+    return w;
+}
+
 fn main() {
     let input = input::file_to_string("day7-input");
     let re = Regex::new(r"^(\w+) \((\d+)\)( -> (.*))?$").unwrap();
@@ -45,7 +88,6 @@ fn main() {
     let mut carried_by = HashMap::new();
     for p in programs.values() {
         for c in p.carry.clone() {
-            //print!("{0} carried by {1}\n", c, p.name.clone());
             carried_by.insert(c, p.name.clone());
         }
     }
@@ -53,6 +95,10 @@ fn main() {
     let mut c = carried_by.keys().nth(0).unwrap().clone();
     while carried_by.contains_key(&c) {
         c = carried_by[&c].clone().to_owned();
+    }
+
+    for child in &programs[&c].carry {
+        calc_weight(child, &programs, 1);
     }
 
     println!("answer day7 part1 = {0}", c);
