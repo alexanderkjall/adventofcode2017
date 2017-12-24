52477b5c4a0adbb05934e2db42c702d5e970e5fb
Alexander Kjäll <alexander.kjall@gmail.com>
2017-12-24 16:03:12 +0100

    solution to day 12 part 2

--- src/day_12.rs
+++ src/day_12.rs
@@ -22,24 +22,11 @@ fn connections(pipes:&Vec<Pipe>, name:&String) -> Vec<String> {
     return to_ret;
 }
 
-fn main() {
-    let input = input::file_to_string("day12-input");
-
-    let re = Regex::new(r"^(\d+) <-> (.*)$").unwrap();
-
-    let mut pipes:Vec<Pipe> = Vec::new();
-    for row in input.split("\n") {
-        for cap in re.captures_iter(&row[..]) {
-            for to in cap[2].split(", ") {
-                pipes.push(Pipe{ from: (&cap[1]).to_owned(), to: to.to_owned() });
-            }
-        }
-    }
-
+fn find_group_size(pipes:&Vec<Pipe>, start:String) -> HashSet<String> {
     let mut to_investigate:Vec<String> = Vec::new();
     let mut investigated:HashSet<String> = HashSet::new();
 
-    to_investigate.push("0".to_owned());
+    to_investigate.push(start);
 
     while to_investigate.len() > 0 {
         let investigate = to_investigate.pop().unwrap();
@@ -54,5 +41,40 @@ fn main() {
         investigated.insert(investigate);
     }
 
+    return investigated;
+}
+
+fn main() {
+    let input = input::file_to_string("day12-input");
+
+    let re = Regex::new(r"^(\d+) <-> (.*)$").unwrap();
+
+    let mut pipes:Vec<Pipe> = Vec::new();
+    for row in input.split("\n") {
+        for cap in re.captures_iter(&row[..]) {
+            for to in cap[2].split(", ") {
+                pipes.push(Pipe{ from: (&cap[1]).to_owned(), to: to.to_owned() });
+            }
+        }
+    }
+
+    let investigated = find_group_size(&pipes, "0".to_owned());
+
     println!("answer day12 part1 = {0}", investigated.len());
+
+    let mut all_names:HashSet<String> = HashSet::new();
+    for p in &pipes {
+        all_names.insert(p.from.clone());
+    }
+
+    let mut groups = 0;
+    while all_names.len() > 0 {
+        let names = find_group_size(&pipes, all_names.iter().nth(0).unwrap().clone());
+        for n in names {
+            all_names.remove(&n);
+        }
+        groups += 1;
+    }
+
+    println!("answer day12 part2 = {0}", groups);
 }
